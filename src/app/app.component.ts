c65db39f195e11b28eec1c6dade8cfe6757a4601
avalojandro <avalojandro@gmail.com>
2022-02-26 17:23:17 -0600

    :sparkles: Angular RichText Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_RichText_Demo';
+
+  //Get div element to pass content to input
+  @ViewChild('richText') richText!: ElementRef;
+
+  //Text formatting function
+    //Initializing variable
+    oDoc:any;
+  formatDoc(cmd:any) {
+  this.oDoc = document.getElementById("textBox");
+  document.execCommand(cmd); this.oDoc?.focus(); 
+  }
+
+  save(){
+    // Open the console to see the output!
+    //You can set this value on an input to send the data in angular forms 
+    console.log(this.richText?.nativeElement.innerHTML);
+  }
+
+
 }
